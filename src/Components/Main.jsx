0a8bd60256f8a379ba08542495e6f66f31e1e9af
Puja kumari <pujakumarinwd0039@gmail.com>
2021-07-06 14:47:51 +0530

    first commit

---
+++ src/Components/Main.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Unsplash from "./unsplash";
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      temp: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ temp: false });
+    this.setState({ search: e.target.value });
+  }
+
+  handleClick() {
+    console.log("yoo");
+    console.log(this.state.search);
+    this.setState({ temp: true });
+    <Unsplash search={this.state.search} />;
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row mb-5">
+          <div className="col-lg-8 mx-auto">
+            <br />
+            <h5></h5>
+            <div className="bg-white p-5 rounded shadow">
+              <div className="p-1 bg-light rounded rounded-pill shadow-sm mb-4">
+                <div className="input-group">
+                  <input
+                    type="search"
+                    placeholder="Search..."
+                    aria-describedby="button-addon1"
+                    className="form-control border-0 bg-light"
+                    onChange={this.handleChange}
+                  />
+                  <div className="input-group-append">
+                    <button
+                      id="button-addon1"
+                      type="submit"
+                      className="btn btn-link text-primary"
+                      onClick={this.handleClick}
+                    >
+                      <i className="fa fa-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mb-5">
+          {this.state.temp ? (
+            <Unsplash search={this.state.search} />
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+export default Main;
---
+++ src/Components/unsplash.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+import axios from "axios";
+class Unsplash extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datas: null,
+      page: 0,
+    };
+    this.loadMore = this.loadMore.bind(this);
+  }
+  componentDidMount() {
+    this.getPhotos();
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.page !== this.state.page) {
+      this.getPhotos();
+    }
+  }
+  async getPhotos() {
+    const { page } = this.state;
+    console.log("hii");
+    await axios
+      .get(
+        `https://api.unsplash.com/search/photos?page=5&per_page=1${page}&query=${this.props.search}&client_id=${process.env.REACT_APP_CLIENT_ID}`
+      )
+      .then((res) => {
+        this.setState({ datas: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("error while fetching data from api");
+      });
+  }
+  loadMore = () => {
+    this.setState((prevState) => ({
+      page: prevState.page + 10,
+    }));
+    console.log(this.state.page);
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.datas ? (
+          <div>
+            <div className="row">
+              {this.state.datas.results.map((data) => {
+                return (
+                  <div key={data.id} className="col-4">
+                    <img
+                      src={data.urls.full}
+                      alt={data.alt_description}
+                      className="img-thumbnail img-fluid"
+                    ></img>
+                  </div>
+                );
+              })}
+            </div>
+            <br />
+            <div className="text-center">
+              <button className="btn btn-secondary" onClick={this.loadMore}>
+                Load more
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    );
+  }
+}
+export default Unsplash;
