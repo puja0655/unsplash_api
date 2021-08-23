d35ec538d798ab55d89251528b96e61af2bb566a
puja0655 <54572747+puja0655@users.noreply.github.com>
2021-08-23 07:04:31 +0530

    google anylatics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 
+import { useEffect } from 'react';
+import ReactGa from 'react-ga'
 import Main from './Components/Main'
 function App() {
+  useEffect(()=>{
+    ReactGa.initialize(process.env.GOOGLE_ANALYTICS_ID)
+    ReactGa.pageview('/')
+  },[])
   return (
     <div className="text-center">
       <Main/>
